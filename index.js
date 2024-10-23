6fdb9ff09f86faeb1f12dac223b6f2c123807492
Jair Josue Heras Campos <al222110834@gmail.com>
2024-10-23 14:52:02 -0600

    Cuarto Commit

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 const express = require("express");
-const app = express();
+const path = require("path"); // Importa path
 const cors = require("cors");
 const { FRONTEND_URL, PORT } = require('./config');
 
-//middleware
+const app = express();
+
+// Middleware
 app.use(express.json());
 app.use(cors({
     origin: FRONTEND_URL
 }));
 
-
-//ROUTES
-
-
-//Registro y Login rutas
 app.use('/auth', require('./routes/encuestaAuth'));
-
 app.use('/dashboard', require('./routes/dashboard'));
-
 app.use('/admin', require('./routes/admin'));
 
+app.use(express.static(path.join(__dirname, 'build'))); 
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html')); 
+});
+
+
 app.listen(PORT, () => {
-    console.log('Server is running on port 5000');
+    console.log('Server is running on port', PORT);
 });
