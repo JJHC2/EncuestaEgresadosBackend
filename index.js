b3ea55f1a2d08d8fa68962b3d199985ec310e026
Jair Josue Heras Campos <al222110834@gmail.com>
2024-10-23 15:02:20 -0600

    Quinto commit

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require("path"); // Importa path
 const cors = require("cors");
 const { FRONTEND_URL, PORT } = require('./config');
 
@@ -8,21 +7,16 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: FRONTEND_URL
+    origin: FRONTEND_URL, 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'] 
 }));
 
+// Rutas
 app.use('/auth', require('./routes/encuestaAuth'));
 app.use('/dashboard', require('./routes/dashboard'));
 app.use('/admin', require('./routes/admin'));
 
-app.use(express.static(path.join(__dirname, 'build'))); 
-
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html')); 
-});
-
-
+// Inicio del servidor
 app.listen(PORT, () => {
-    console.log('Server is running on port', PORT);
+    console.log(`Server is running on port ${PORT}`);
 });
