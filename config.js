ef8f73f0f1be5803c32ea1194167524492e68e05
Ricardo Antonio Carrillo Perez <carrilloricardo588@gmail.com>
2024-11-05 22:11:13 -0600

    Cambios en config

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
 const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || 5433;
 const DB_USER = process.env.DB_USER || 'postgres';
-const DB_PASSWORD = process.env.DB_PASSWORD || '12345678';
+const DB_PASSWORD = process.env.DB_PASSWORD || '';
 const DB_DATABASE = process.env.DB_DATABASE || 'encuesta_egresados';
 const PORT = process.env.PORT || 5000;
 
