c91932ceb9334710516a9e7751216b0ef67fb570
Jair Campos <al222110834@utvtol.edu.mx>
2024-11-10 14:36:46 -0600

    Cambios para el despliegue en render

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-const FRONTEND_URL = process.env.FRONTEND_URL || 'https://encuesta-frontend.onrender.com'; 
-
 const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || 5433;
 const DB_USER = process.env.DB_USER || 'postgres';
@@ -8,7 +6,6 @@ const DB_DATABASE = process.env.DB_DATABASE || 'encuesta_egresados';
 const PORT = process.env.PORT || 5000;
 
 module.exports = {
-    FRONTEND_URL,
     DB_HOST,
     DB_PORT,
     DB_USER,
--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 const express = require("express");
 const cors = require("cors");
-const { PORT } = require('./config');
-;
-
+const { PORT } = require("./config");
 const app = express();
 
 
-app.use(cors());
+const corsOptions ={
+    origin: process.env.FRONTEND_URL,
+    optionSuccessStatus: 200
+}
 
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
-
-app.use('/auth', require('./routes/encuestaAuth'));
-app.use('/dashboard', require('./routes/dashboard'));
-app.use('/admin', require('./routes/admin'));
-app.use('/profile', require('./routes/Profile'));
-
-
-
+app.use("/auth", require("./routes/encuestaAuth"));
+app.use("/dashboard", require("./routes/dashboard"));
+app.use("/admin", require("./routes/admin"));
+app.use("/profile", require("./routes/Profile"));
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
